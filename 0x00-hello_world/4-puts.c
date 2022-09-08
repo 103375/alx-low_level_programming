49f29007efa3a9815e68056bea9137b8c2cfe634
root <thembamashabane0@gmail.com>
2022-09-08 16:45:13 -0700

    Programming is like building

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+/**
+ * main
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(voi)
+{
+	puts ("/"Programing is like building a multilingula puzzle");
+	return (0);
+}
