1e0a8749f38574c4b0b65ea91f58998ff42f0154
root <thembamashabane0@gmail.com>
2022-09-12 06:38:31 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,4 @@
-#include <stdio>                                                                                                                       
+#include <stdio.h>                                                                                                                       
 /**                                                                                                                                     
  * main - Entry point                                                                                                                   
  *   
@@ -17,6 +17,6 @@ for (upper = 'A'; upper <= 'Z'; upper++)
 {
 putchar(upper);
 }
-printf('\n');
+putchar('\n');
 return (0);	 
 }
