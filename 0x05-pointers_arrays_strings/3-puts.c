e47145d954b615dc93449a8786e7cd061778c208
root <thembamashabane0@gmail.com>
2022-09-19 10:22:39 -0700

    3 puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,12 +9,13 @@
 void _puts(char *str)
 
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*(str + i) != '\0')
 	{
 	putchar(*(str + i));
+	i++;
 	}
 
-	putchar('\n');
+	putchar(10);
 }
