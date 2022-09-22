ce660035ed44177e3826edbf322b938aa72c273a
root <thembamashabane0@gmail.com>
2022-09-22 05:01:02 -0700

    0 strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,7 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * _strcat -> this is a function strcat
+ * _strcat - this is a function strcat
  * @dest: first param
  * @src: second param
  * Return: a string
@@ -10,15 +9,11 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int len = 0, i;
+	int index = 0, dest_len = 0;
 
-	while (dest[len])
-		len++;
-	for (i = 0; src[i] != '0'; i++)
-	{
-		dest[len] = src[i];
-		len += 1;
-	}
-	dest[len] = '\0';
+	while (dest[index++]
+			dest_len++;
+	for (index = 0; src[index]; index++)
+	dest[dest_len++] = src[index]
 	return (dest);
 }
