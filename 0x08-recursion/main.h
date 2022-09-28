015c4e94293118bde3f5a3a70cc40534c59217a1
root <thembamashabane0@gmail.com>
2022-09-28 14:14:31 -0700

    101 wildcmp

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,6 +13,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-int helper_pal(char *str, int length, int count)
-int strlen_rec(char *str)
+
 #endif /*MAIN_H*/
