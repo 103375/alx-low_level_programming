f45678723cae5ac942679175b399161462e8e15d
root <thembamashabane0@gmail.com>
2022-10-04 06:13:13 -0700

    0 create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,52 +1,36 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * create_array - Returns a pointer to a newly allocated space in memory.
  *
- * Return: Nothing.
+ * @size: unsigned int
+ *
+ * @c: char
+ *
+ * Return: char pointer
  */
 
-void simple_print_buffer(char *buffer, unsigned int size)
+char *create_array(unsigned int size, char c)
 {
-    unsigned int i;
+	unsigned int counter;
+	char *character;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-    char *buffer;
-
-    buffer = create_array(98, 'H');
-    if  (buffer == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    simple_print_buffer(buffer, 98);
-    free(buffer);
-    return (0);
+	counter = 0;
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	character = malloc(sizeof(char) * size);
+	if (character == NULL)
+	{
+		return (NULL);
+	}
+	while (counter < size)
+	{
+		character[counter] = c;
+		counter++;
+	}
+	character[counter] = '\0';
+	return (character);
 }
