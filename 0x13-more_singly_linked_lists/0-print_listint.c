b6b93075e6530955ef04a136664b9f145aaaaed3
root <thembamashabane0@gmail.com>
2022-10-24 12:31:14 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	unsigned int index = 0;
+	int = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		printf("%i\n", h->n);
 		h = h->next;
-		index++;
+		i++;
 	}
-	return (index);
+	return (i);
 }
