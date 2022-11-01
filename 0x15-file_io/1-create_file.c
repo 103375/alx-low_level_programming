1c45fee0b08a935326f168f9f57ead1e245bd9ea
root <thembamashabane0@gmail.com>
2022-10-31 17:04:53 -0700

    1 create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,35 +10,19 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd_open, fd_write;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fd_open = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
-	if (fd_open == -1)
-		return (-1);
 	if (text_content != NULL)
 	{
-		fd_write = write(fd_open, text_content, _strlen(text_content));
-		if (fd_write == -1)
-			return (-1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	close(fd_open);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+	if (o == -1 || w == -1)
+		return (-1);
+	close(o);
 	return (1);
-}
-
-/**
- * _strlen - calculate the lenght of a string.
- * @str: array of characters.
- *
- * Return: lenght of the string.
- */
-
-int _strlen(char *str)
-{
-	int count = 0;
-
-	while (*str++)
-		count++;
-	return (count);
 }
